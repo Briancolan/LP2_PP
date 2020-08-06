eefab4efdc40d34878acdd056d98ed698e613195
Briancolan <68067899+Briancolan@users.noreply.github.com>
2020-08-06 17:00:13 -0500

    Update Editorial.java

--- src/java/Modelo/Editorial.java
+++ src/java/Modelo/Editorial.java
@@ -1,26 +1,23 @@
 package Modelo;
-
 public class Editorial {
     private int ideditorial;
     private String nombre;
     private String estado;
+    private int inicioed;
+    private String reseñas;
+    private String convenios;
+    
 
     public Editorial() {
     }
-    
 
-    public Editorial(int ideditorial, String nombre, String estado) {
+    public Editorial(int ideditorial, String nombre, String estado, int inicioed, String reseñas, String convenios) {
         this.ideditorial = ideditorial;
         this.nombre = nombre;
         this.estado = estado;
-    }
-
-    public String getEstado() {
-        return estado;
-    }
-
-    public void setEstado(String estado) {
-        this.estado = estado;
+        this.inicioed = inicioed;
+        this.reseñas = reseñas;
+        this.convenios = convenios;
     }
 
     public int getIdeditorial() {
@@ -38,5 +35,37 @@ public class Editorial {
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-      
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public int getInicioed() {
+        return inicioed;
+    }
+
+    public void setInicioed(int inicioed) {
+        this.inicioed = inicioed;
+    }
+
+    public String getReseñas() {
+        return reseñas;
+    }
+
+    public void setReseñas(String reseñas) {
+        this.reseñas = reseñas;
+    }
+
+    public String getConvenios() {
+        return convenios;
+    }
+
+    public void setConvenios(String convenios) {
+        this.convenios = convenios;
+    }
+
 }
